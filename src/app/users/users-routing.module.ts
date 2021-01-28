bbb294f1c64fcc3bbca81da622a7ecbfcacea32c
Oriol <magna.OMIRO.shogun@gmail.com>
2021-01-28 20:20:35 +0100

    Resolve users, show user table

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,12 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { Injectable, NgModule } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../data/user.interface';
+import { UsersRESTService } from '../services/users-rest.service';
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+@Injectable()
+export class UsersResolver implements Resolve<Observable<User[]>> {
+  constructor(private usersRESTService: UsersRESTService) {}
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<User[]> {
+    // Add some SEO
+    return this.usersRESTService.getAllUsers();
+  }
+}
+
+const routes: Routes = [
+  { path: '', component: UsersComponent, resolve: { users: UsersResolver } },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [UsersResolver]
 })
-export class UsersRoutingModule { }
+export class UsersRoutingModule {}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../data/user.interface';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.users = this.route.snapshot.data['users'];
+  }
 
   ngOnInit(): void {}
 }
