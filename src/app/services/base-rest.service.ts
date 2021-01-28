671de6eaf959e66241bcdc55f322fbe923c84e8d
Oriol <magna.OMIRO.shogun@gmail.com>
2021-01-28 19:53:32 +0100

    Base REST service

---
+++ src/app/services/base-rest.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BaseRESTService {
+  private apiURL: string;
+
+  constructor(private http: HttpClient) {
+    this.apiURL = environment.baseURL;
+  }
+
+  getAll<T>(
+    path: string,
+    { page, limit }: { page: number; limit: number } = { page: 0, limit: 10 }
+  ): Observable<T[]> {
+    return this.http
+      .get<T[]>(`${this.apiURL}/${path}`)
+      .pipe(map((list) => list.slice(page * limit, page * limit + limit)));
+  }
+}
